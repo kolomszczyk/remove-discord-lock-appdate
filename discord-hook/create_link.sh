22b01f85c79d8fc5d10ac03817df30ee35dcc8eb
kolomszczyk <kolomszczyk@gmail.com>
2022-10-27 02:24:56 +0200

    discord-update-setting.sh done

--- discord-hook/create_link.sh
+++ discord-hook/create_link.sh
@@ -2,3 +2,9 @@
 # /usr/share/liblpm/hooks
 # /etc/pacman.d/hooks/
 #
+#
+# dependence
+# jq <- manipulate json
+#
+#
+dirname "$0"
---
+++ discord-hook/discord-update-setting.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+JSON_PATH="$(getent passwd $SUDO_USER|cut -d: -f6)/Work/discord-hook/settings.json"
+
+
+JQ_EXPRESION='del(.SKIP_HOST_UPDATE) | . + {"SKIP_HOST_UPDATE": true}'
+
+# adds {SKIP_HOST_UPDATE: true} to discord setting
+# https://wiki.archlinux.org/title/Discord#Installation
+jq "$JQ_EXPRESION" $JSON_PATH \
+	> $JSON_PATH.tmp && mv $JSON_PATH.tmp $JSON_PATH
+
+chown $SUDO_USER:$SUDO_USER $JSON_PATH
